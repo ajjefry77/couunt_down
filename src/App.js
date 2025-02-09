cf185a9798544d1ae5029489a4fd72e216ecb64d
arad <arardjafary@gmail.com>
2025-02-09 12:41:25 +0330

    tamrin 2 button

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Timer from './Timer';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Timer from "./Timer";
 
 class App extends React.Component {
-    render() {
-        return(
-            <div>
-                <h1>
-                    time is
-                </h1>
-                <Timer/>
-            </div>
-        )
-    }
+  
+
+  render() {
+    return (
+      <div>
+        <h1>
+          <Timer />
+        </h1>
+        
+      </div>
+    );
+  }
 }
 
-export default App
+export default App;
--- src/Timer.js
+++ src/Timer.js
@@ -1,41 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-var interval
+var interval;
 
-class Timer  extends React.Component {
-    constructor(){
-        super();
-        this.state = {
-            timer_state : 5
+class Timer extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      timer_state: 5,
+    };
+  }
 
-        };
-    }
+  start_interval = () => {
+    interval = setInterval(() => {
+      this.setState({
+        timer_state: this.state.timer_state - 1,
+      });
+    }, 1000);
+  };
 
-    componentDidMount(){
-        interval = setInterval(() => {
-            this.setState({
-                timer_state: this.state.timer_state - 1
-            })
-        }, 1000)
-    }
+  stop_interval = () => {
+    clearInterval(interval);
+  };
+  componentDidMount(){
+    this.start_interval()
+  }
 
-    componentDidUpdate(){
-        console.log("ji")
-        if(this.state.timer_state === 0){
-            clearInterval(interval)
-        }
-    }
+ componentDidUpdate(){
+   if(this.state.timer_state == 0){
+    this.stop_interval()
+   }
+ }
 
-    render(){
-        return(
-            <h2>
-                {this.state.timer_state}
-            </h2>
-        )
-    }
+  
+  
 
-
+  render() {
+    return (
+      <>
+        <h2>time is {this.state.timer_state}</h2>
+        <button onClick={this.start_interval}>start</button>
+        <button onClick={this.stop_interval}>stop</button>
+        <button onClick={}>reset</button>
+      </>
+    );
+  }
 }
 
-export default Timer
+export default Timer;
