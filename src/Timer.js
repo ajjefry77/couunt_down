2512e17d3d3731347381d1ce90e1d239f7993d5d
arad <arardjafary@gmail.com>
2025-02-09 19:18:26 +0330

    stop watch

--- src/Timer.js
+++ src/Timer.js
@@ -7,41 +7,70 @@ class Timer extends React.Component {
   constructor() {
     super();
     this.state = {
-      timer_state: 5,
+      hour: 0,
+      minute: 0,
+      second: 0,
+      isStart: false,
     };
   }
 
   start_interval = () => {
-    interval = setInterval(() => {
-      this.setState({
-        timer_state: this.state.timer_state - 1,
-      });
-    }, 1000);
+    if (this.state.isStart == false) {
+        this.setState({
+            isStart: true,
+        })
+      interval = setInterval(() => {
+        this.setState({
+          second: this.state.second + 1,
+        });
+
+        if (this.state.second == 60) {
+          this.setState({
+            second: 0,
+            minute: this.state.minute + 1,
+          });
+        }
+
+        if (this.state.minute == 60) {
+          this.setState({
+            minute: 0,
+            hour: this.state.hour + 1,
+          });
+        }
+      }, 1000);
+    }
   };
 
   stop_interval = () => {
+    this.setState({
+        isStart: false
+    })
     clearInterval(interval);
   };
-  componentDidMount(){
-    this.start_interval()
-  }
 
- componentDidUpdate(){
-   if(this.state.timer_state == 0){
-    this.stop_interval()
-   }
- }
-
-  
-  
+  reset_interval = () => {
+    clearInterval(interval);
+    this.setState({
+      hour: 0,
+      minute: 0,
+      second: 0,
+      isStart: false
+    });
+  };
 
   render() {
+    var h = this.state.hour
+    var m = this.state.minute
+    var s = this.state.second
+
     return (
       <>
-        <h2>time is {this.state.timer_state}</h2>
+        <h2>
+          time is {h > 9 ? h : "0" + h}:{m > 9 ? m : "0" + m}:{s > 9 ? s : "0" + s}
+        </h2>
         <button onClick={this.start_interval}>start</button>
         <button onClick={this.stop_interval}>stop</button>
-        <button onClick={}>reset</button>
+        <button onClick={this.reset_interval}>reset</button>
       </>
     );
   }
